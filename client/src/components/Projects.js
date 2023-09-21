6c90ed82a434a04ebdd0d4933563005b7ca7c820
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-09-22 01:38:17 +0530

    Update Projects.js

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react';
-import {TextField, Button, Container, Divider} from '@material-ui/core';
-import {Card, CardHeader, CardContent} from '@material-ui/core';
-import axios from 'axios';
-import {saveAs} from 'file-saver';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import LinkIcon from '@material-ui/icons/Link';
-import TitleIcon from '@material-ui/icons/Title';
-import DescriptionIcon from '@material-ui/icons/Description';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import {Row, Col} from 'react-bootstrap';
-import {Paper, withStyles, Grid} from '@material-ui/core';
+import React from "react";
+import { TextField, Button, Container, Divider } from "@material-ui/core";
+import { Card, CardHeader, CardContent } from "@material-ui/core";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import LinkIcon from "@material-ui/icons/Link";
+import TitleIcon from "@material-ui/icons/Title";
+import DescriptionIcon from "@material-ui/icons/Description";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { Row, Col } from "react-bootstrap";
+import { Paper, withStyles, Grid } from "@material-ui/core";
 
-const styles = theme => ({
+const styles = (theme) => ({
   margin: {
     margin: theme.spacing.unit * 1.5,
   },
@@ -21,271 +21,268 @@ const styles = theme => ({
   },
 });
 
-class Projects extends Component {
-  continue = e => {
-    e.preventDefault ();
-    this.props.nextStep ();
+function Projects(props) {
+  const continueStep = (e) => {
+    e.preventDefault();
+    props.nextStep();
   };
 
-  back = e => {
-    e.preventDefault ();
-    this.props.prevStep ();
+  const goBack = (e) => {
+    e.preventDefault();
+    props.prevStep();
   };
 
-  createAndDownloadPDF = () => {
+  const createAndDownloadPDF = () => {
     axios
-      .post ('/create-pdf', this.state)
-      .then (() => {
+      .post("/create-pdf", props.values)
+      .then(() => {
         axios
-          .get ('fetch-pdf', {responseType: 'blob'})
-          .then (res => {
-            const pdfBlob = new Blob ([res.data], {type: 'application/pdf'});
-            saveAs (pdfBlob, 'Resume.pdf');
+          .get("fetch-pdf", { responseType: "blob" })
+          .then((res) => {
+            const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+            saveAs(pdfBlob, "Resume.pdf");
           })
-          .catch (err => {
-            console.log (err);
+          .catch((err) => {
+            console.log(err);
           });
       })
-      .catch (err => {
-        console.log (err);
+      .catch((err) => {
+        console.log(err);
       });
   };
 
-  render () {
-    const {values} = this.props;
-    const {classes} = this.props;
+  const { values, classes } = props;
 
-    return (
-      <Paper className={classes.padding}>
-        <Card>
-          <CardHeader title="Projects Developed" />
-        </Card>
-        <CardContent>
-          <div className={classes.margin}>
-            <Grid container spacing={2} alignItems="center" lg={12}>
-              <Grid item xs={12} lg={12}>
-                <h5>Project 1</h5>
-              </Grid>
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="title1"
-                  label="Title"
-                  style={{width: '80%'}}
-                  required
-                  value={values.title1}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <TitleIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-              <Grid item md={4} sm={6} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="link1"
-                  label="Link"
-                  style={{width: '80%'}}
-                  required
-                  value={values.link1}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <LinkIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="projectDescription1"
-                  label="Description"
-                  style={{width: '80%'}}
-                  required
-                  value={values.projectDescription1}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <DescriptionIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
+  return (
+    <Paper className={classes.padding}>
+      <Card>
+        <CardHeader title="Projects Developed" />
+      </Card>
+      <CardContent>
+        <div className={classes.margin}>
+          <Grid container spacing={2} alignItems="center" lg={12}>
+            {/* Project 1 */}
+            <Grid item xs={12} lg={12}>
+              <h5>Project 1</h5>
             </Grid>
-            <br />
-            <Divider />
-            <br />
-            <Grid container spacing={2} alignItems="center" lg={12}>
-              <Grid item xs={12} lg={12}>
-                <h5>Project 2</h5>
-              </Grid>
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="title2"
-                  label="Title"
-                  style={{width: '80%'}}
-                  required
-                  value={values.title2}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <TitleIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-              <Grid item md={4} sm={6} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="link2"
-                  label="Link"
-                  style={{width: '80%'}}
-                  required
-                  value={values.link2}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <LinkIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="projectDescription2"
-                  label="Description"
-                  style={{width: '80%'}}
-                  required
-                  value={values.projectDescription2}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <DescriptionIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="title1"
+                label="Title"
+                style={{ width: "80%" }}
+                required
+                value={values.title1}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <TitleIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
-            <br />
-            <Divider />
-            <br />
-            <Grid container spacing={2} alignItems="center" lg={12}>
-              <Grid item xs={12} lg={12}>
-                <h5>Project 3</h5>
-              </Grid>
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="title3"
-                  label="Title"
-                  style={{width: '80%'}}
-                  value={values.title3}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <TitleIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="link3"
-                  label="Link"
-                  style={{width: '80%'}}
-                  value={values.link3}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <LinkIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-
-              <Grid item md={12} sm={12} xs={12} lg={12}>
-                <TextField
-                  margin="dense"
-                  variant="outlined"
-                  name="projectDescription3"
-                  label="Description"
-                  style={{width: '80%'}}
-                  value={values.projectDescription3}
-                  onChange={this.props.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <DescriptionIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
+            <Grid item md={4} sm={6} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="link1"
+                label="Link"
+                style={{ width: "80%" }}
+                required
+                value={values.link1}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <LinkIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
-          </div>
-        </CardContent>
-        <Container className={classes.margin}>
-          <Row>
-            <Col xs={4} />
-            <Col xs={2}>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={this.back}
-                startIcon={<NavigateBeforeIcon />}
-              >
-                Back
-              </Button>
-            </Col>
-            <Col xs={2}>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={this.continue}
-                endIcon={<NavigateNextIcon />}
-              >
-                Next
-              </Button>
-            </Col>
-            <Col xs={4} />
-          </Row>
-        </Container>
-        <p className="text-center text-muted">Page 3</p>
-      </Paper>
-    );
-  }
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="projectDescription1"
+                label="Description"
+                style={{ width: "80%" }}
+                required
+                value={values.projectDescription1}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <DescriptionIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+          <br />
+          <Divider />
+          <br />
+          <Grid container spacing={2} alignItems="center" lg={12}>
+            {/* Project 2 */}
+            <Grid item xs={12} lg={12}>
+              <h5>Project 2</h5>
+            </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="title2"
+                label="Title"
+                style={{ width: "80%" }}
+                required
+                value={values.title2}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <TitleIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item md={4} sm={6} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="link2"
+                label="Link"
+                style={{ width: "80%" }}
+                required
+                value={values.link2}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <LinkIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="projectDescription2"
+                label="Description"
+                style={{ width: "80%" }}
+                required
+                value={values.projectDescription2}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <DescriptionIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+          <br />
+          <Divider />
+          <br />
+          <Grid container spacing={2} alignItems="center" lg={12}>
+            {/* Project 3 */}
+            <Grid item xs={12} lg={12}>
+              <h5>Project 3</h5>
+            </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="title3"
+                label="Title"
+                style={{ width: "80%" }}
+                value={values.title3}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <TitleIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="link3"
+                label="Link"
+                style={{ width: "80%" }}
+                value={values.link3}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <LinkIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={12}>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                name="projectDescription3"
+                label="Description"
+                style={{ width: "80%" }}
+                value={values.projectDescription3}
+                onChange={props.handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <DescriptionIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </CardContent>
+      <Container className={classes.margin}>
+        <Row>
+          <Col xs={4} />
+          <Col xs={2}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={goBack}
+              startIcon={<NavigateBeforeIcon />}
+            >
+              Back
+            </Button>
+          </Col>
+          <Col xs={2}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={continueStep}
+              endIcon={<NavigateNextIcon />}
+            >
+              Next
+            </Button>
+          </Col>
+          <Col xs={4} />
+        </Row>
+      </Container>
+      <p className="text-center text-muted">Page 3</p>
+    </Paper>
+  );
 }
 
-export default withStyles (styles) (Projects);
+export default withStyles(styles)(Projects);
